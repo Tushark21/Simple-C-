bc378d3b3975199eeb802fc8910e6dc05714ebf1
Tushark21 <tushargautamk21@gmail.com>
2017-09-18 15:43:19 +0530

    Program to find Square Root of a Number

---
+++ Square Root.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <math.h>
+#include <conio.h>
+
+using namespace std;
+
+int main(){
+	
+	float a,root;
+
+	cout<<"Enter Number\n";
+	cin>>a;
+
+	root=sqrt(a);
+	cout<<"Root of "<<a<<"="<<root;
+
+	getch();
+	return 0;
+}
